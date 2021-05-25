d1794b74341237cf228f46c994eacb0b77c1a687
Dinesh <pydinesh3@gmail.com>
2021-05-25 19:58:56 +0530

    Create 04 Whileloop.js

---
+++ JS/04 Whileloop.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        var count=10;
+
+        while (count>0) {
+            console.log("Count: "+ count--);
+        }
+        console.log("The End");
+    </script>
+</head>
+<body>
+<h1>While loop</h1>    
+</body>
+</html>

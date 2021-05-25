0d7f86bfe88f3d69ea2dd69e3bf3f97f821d08ef
Dinesh <pydinesh3@gmail.com>
2021-05-25 19:29:47 +0530

    Update and rename 02 Arithmetic .js to 02 Base .js

--- JS/02 Arithmetic .js
+++
@@ -1,6 +0,0 @@
-Objects
-var age=40;
-var age='I am Dinesh';
------------------------------ type of operator ==null (object it was wrong)
-(type of age):==>Data type 
------------------------------
---
+++ JS/02 Base .js
@@ -0,0 +1,9 @@
+Alret: message display
+
+alert('Hello JS');
+==========================================
+Prompt: user kitta irrunthu input vangum
+
+var name= prompt('what is ur name', 'anonymous');
+alrert('Hai'+name);
+==========================================

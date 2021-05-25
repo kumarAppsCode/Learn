805cf598044fdeee7925529a84a573346fff10b4
Dinesh <pydinesh3@gmail.com>
2021-05-25 20:03:39 +0530

    Create 05 For Loop.js

---
+++ JS/05 For Loop.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        for (var daysinWeek = 1; daysinWeek <=7; daysinWeek++) {
+            console.log("Days in Week :"+daysinWeek);
+        }
+        console.log("The End");
+    </script>
+
+</head>
+<body>
+    For Loop
+</body>
+</html>

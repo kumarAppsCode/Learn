6df15738717f4a4808bb59838b3d21611f21c164
Dinesh <pydinesh3@gmail.com>
2021-05-25 19:41:56 +0530

    Update 02 Base .js

--- JS/02 Base .js
+++ JS/02 Base .js
@@ -11,3 +11,29 @@ confirm: ok and cancel popup will come
 var name =confirm('I am ok');
 alert(name);
 ==========================================
+
+<html>
+<head>
+<title>Testing</title>
+<!-- <script src="sample.js"></script> -->
+<script>
+    var age=prompt("Enter the age", 18);
+    alert(age);
+    console.log(age);
+
+var parentPermission=true;
+    if(age>18 && parentPermission){
+        alert("Permission Granded")
+    }else{
+        alert("Permission decline")
+    }
+</script>
+
+
+
+</head>
+<body>
+<p>Welcome to the Testing </p>
+</body>
+</html>  
+  

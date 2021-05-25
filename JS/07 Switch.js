b69b14a01b5c62d193f488dbe829768c48f999c3
Dinesh <pydinesh3@gmail.com>
2021-05-25 21:10:03 +0530

    Create 07 Switch.js

---
+++ JS/07 Switch.js
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+
+        days =5;
+        // alert(days);
+        console.log(days);
+        switch (days) {
+            case 1:
+                console.log("Monday");
+                // alert("Today Monday");
+                break;
+            case 2:
+                console.log("Tuesday");
+                // alert("Today Tuesday");
+                break;
+            case 3:
+                console.log("Wednesday");
+                // alert(confirm("Today Wednesday"));
+                break;
+            case 4:
+                console.log("Thursday");
+                // alert(confirm("Today Thursday"));
+                break;
+            case 5:
+                console.log("Friday");
+                // alert(confirm("Today Friday"));
+                break;
+            case 6:
+                console.log("Saturday");
+                // alert(confirm("Today Saturday"));
+                break;
+            case 7:
+                console.log("Sunday");
+                // alert(confirm("Today Sunday"));
+                break;
+            default:
+                console.log("Error");
+                break;
+        }
+    </script>
+ 
+
+</head>
+
+<body>
+    Switch Statment
+</body>
+
+</html>

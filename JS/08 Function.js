a7111907dabdc87c6a2312e00c19eaf914b93b6f
Dinesh <pydinesh3@gmail.com>
2021-05-25 21:24:39 +0530

    Create 08 Function.js

---
+++ JS/08 Function.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Function nanme
+      Function functionName (param 1, param 2){
+          block statement
+      }  
+    functionName(arg1, arg2);
+    --> 
+    <script>
+        function message() {
+            console.log("Welcome Team");
+        }
+        message();
+    </script>
+</head>
+<body>
+    
+</body>
+</html>

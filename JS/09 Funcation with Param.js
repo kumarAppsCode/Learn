9b1a59a3b7395eb712302076f4195a848a1f5c7b
Dinesh <pydinesh3@gmail.com>
2021-05-25 21:34:24 +0530

    Create 09 Funcation with Param.js

---
+++ JS/09 Funcation with Param.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <Script>
+        function method(message, name) {
+          return   "Hellow "+" "+name+" "+message;
+        }
+
+        var a=method("How are you", "Dinesh");
+        console.log(a);
+
+    </Script>
+</head>
+<body>
+Function with Param    
+</body>
+</html>

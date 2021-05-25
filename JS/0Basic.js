e49081976487ed4d77dee635140d8f530ac37818
Dinesh <pydinesh3@gmail.com>
2021-05-25 18:34:29 +0530

    Update 0Basic.js

--- JS/0Basic.js
+++ JS/0Basic.js
@@ -1,1 +1,28 @@
-
+Dial up Internet Access: Internet Explorer(jQuery) and Navigator Broser
+10 Day develop : life Script==>JSScript
+European Comupter Manufactor Asscociation Script Vs JsScript;
+ECMA Script
+ES2
+ES3
+ES5
+ES6
+************************************************************************
+ECMA Script-Standard
+Js- Standard based language
+ECMA Script:
+          DOM--
+          BOM--
+************************************************************************
+JS:
+TAg: <script> </script> (Internal and external)
+External
+<script> ---scr
+<script> ---type=text/javascript
+************************************************************************
+<script> tag used:=> Header
+<script> tag used:=> body tag
+mostly use in body tag to avoid rendering
+************************************************************************
+ Analytics script( Analytics script= page visitor) not use in body because person may close the page
+ so we need to add analytics script in header
+************************************************************************

1399fc8b9d37ac7eba657805ecec91a8a85627e6
Dinesh <pydinesh3@gmail.com>
2021-06-22 20:07:36 +0530

    Update 1 Current Date.js

--- VBCS/Js/1 Current Date.js
+++ VBCS/Js/1 Current Date.js
@@ -14,3 +14,19 @@
   // window.alert(date);
   return date;
   };
+******************************************************************************************
+    /**
+   *
+   * @param {String} arg1
+   * @return {String}
+   */
+  AppModule.prototype.getSysDate = function (arg1) {
+  var today = new Date();
+  // window.alert(today);
+  // var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+  var date =today.getFullYear()+'-'+ ('0'+(today.getMonth()+1)).slice(-2) +'-'+ ('0'+today.getDate()).slice(-2);
+  // console.log(date);
+  // window.alert(date);
+  return date;
+  };
+******************************************************************************************

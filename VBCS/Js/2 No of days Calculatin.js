8f94ed08fa18becc5f078071065b3fb743b02536
Dinesh <pydinesh3@gmail.com>
2021-06-22 20:04:52 +0530

    Create 2 No of days Calculatin.js

---
+++ VBCS/Js/2 No of days Calculatin.js
@@ -0,0 +1,18 @@
+  /**
+   *
+   * @param {String} arg1
+   * @return {String}
+   */
+ PageModule.prototype.getNoOfDays = function (sdate, edate) {
+
+  window.alert("Start Date"+sdate);
+  window.alert("End Date"+edate);
+
+  var date1 = new Date(sdate);
+  var date2 = new Date(edate);
+  var Difference_In_Time = date2.getTime() - date1.getTime();
+  var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+ 
+  window.alert("total no of days: "+Difference_In_Time);
+  return Difference_In_Days;
+  };

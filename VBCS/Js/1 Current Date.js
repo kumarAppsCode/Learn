be6f5df01b65311165e4b964ec2f31fa70c489a1
Dinesh <pydinesh3@gmail.com>
2021-06-22 19:36:55 +0530

    Update 1 Current Date.js

--- VBCS/Js/1 Current Date.js
+++ VBCS/Js/1 Current Date.js
@@ -2,12 +2,15 @@
    *
    * @param {String} arg1
    * @return {String}
+   *{"type": "datetime", "options": {"pattern":"MM/dd/yyyy"}}
    */
+
   PageModule.prototype.getSysDate = function (arg1) {
   var today = new Date();
   // window.alert(today);
   // var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
-  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  var date =today.getFullYear()+'-'+ ('0'+(today.getMonth()+1)).slice(-2) +'-'+ ('0'+today.getDate()).slice(-2);
+  // console.log(date);
   // window.alert(date);
   return date;
   };

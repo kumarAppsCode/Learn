75f9c13bbf81444ba765d8625aef57b20f3f3f04
Dinesh <pydinesh3@gmail.com>
2021-08-25 17:28:52 +0530

    Update 0Basic.js

--- JS/01 JS2/Primitive Type/0Basic.js
+++ JS/01 JS2/Primitive Type/0Basic.js
@@ -1,1 +1,26 @@
-hi
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script>
+        var customerName="Dinesh";
+        var customerBalance=200;
+        var isValid=true;
+        var object=null;
+        var x=undefined;
+        var y;
+        document.write(customerName +"<br/>");
+        document.write(customerBalance +"<br/>");
+        document.write(isValid +"<br/>");
+        document.write(object +"<br/>");
+        document.write(x +"<br/>");
+        document.write(y +"<br/>");
+    </script>
+</head>
+<body>
+    
+</body>
+</html>

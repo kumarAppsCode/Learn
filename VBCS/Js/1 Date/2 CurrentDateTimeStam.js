3d793a2f79111db6127d47585c7b6dabe80d9819
Dinesh <pydinesh3@gmail.com>
2022-03-04 21:58:11 +0530

    Create 2 CurrentDateTimeStam.js

---
+++ VBCS/Js/1 Date/2 CurrentDateTimeStam.js
@@ -0,0 +1,17 @@
+  PageModule.prototype.systemDate = function() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy+'-'+mm+'-'+dd+'T00:00:00Z'; 
+    return today;
+  };
+
+  PageModule.prototype.sysDates = function() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+    today = yyyy+'-'+mm+'-'+dd; 
+    return today;
+  };

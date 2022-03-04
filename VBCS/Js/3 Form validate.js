591a02179bc248fac5e6ad2544d9a7829b70a57b
Dinesh <pydinesh3@gmail.com>
2022-03-04 21:41:57 +0530

    Update 3 Form validate.js

--- VBCS/Js/3 Form validate.js
+++ VBCS/Js/3 Form validate.js
@@ -1,15 +1,36 @@
-  /**
-   *
-   * @param {String} arg1 component id
-   * @return {String}
-   */
-  PageModule.prototype.validateForm = function (arg1) {
-    var myForm = document.getElementById(arg1);
-    if (myForm.valid === "valid") {
-      return true;
-    } else {
-      myForm.showMessages();
-      myForm.focusOn("@firstInvalidShown");
-      return false;
-    }
-  };
+/*
+  Copyright (c) 2018, Oracle and/or its affiliates.
+  The Universal Permissive License (UPL), Version 1.0
+*/
+define([], function () {
+  'use strict';
+
+var AppModule = function AppModule() {};
+
+AppModule.prototype.isFormValid = function(form) {
+var tracker = document.getElementById(form); 
+if (tracker.valid === "valid") {
+  return true;
+} else {
+tracker.showMessages();
+tracker.focusOn("@firstInvalidShown");
+return false;
+}
+};
+
+  return AppModule;
+});
+
+
+// {{$application.functions.isFormValid("CreateForm")}}
+//  <oj-validation-group id="CreateForm">
+//  <div class="oj-flex">
+// <oj-form-layout  label-edge="start" max-columns="2" class="oj-flex-item oj-sm-12 oj-md-12">
+//   <oj-label for="oj-input-text--1072052825-1" class="oj-flex-item oj-sm-12 oj-md-2">UserName</oj-label>
+//   <oj-input-text id="oj-input-text--1072052825-1" class="oj-flex-item oj-sm-12 oj-md-4" required="true"></oj-input-text>
+//   <oj-label for="oj-input-number--1072052825-1" class="oj-flex-item oj-sm-12 oj-md-4">Mobile No</oj-label>
+//   <oj-input-number id="oj-input-number--1072052825-1" class="oj-flex-item oj-sm-12 oj-md-8" required="true"></oj-input-number>
+//   <oj-input-date label-hint="StartDate" required="true"></oj-input-date>
+// </oj-form-layout>
+//  </div>
+//   </oj-validation-group>

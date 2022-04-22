681e119a8fdcf614add823559265a92073412aa4
Dinesh <pydinesh3@gmail.com>
2022-04-22 18:16:50 +0300

    Create 0 Base Save.js

---
+++ VBCS/04 Download/01 File Download/Js/0 Base Save.js
@@ -0,0 +1,34 @@
+REST-->Save
+/*
+  Copyright (c) 2018, Oracle and/or its affiliates.
+  The Universal Permissive License (UPL), Version 1.0
+*/
+define([], function() {
+  'use strict';
+
+  var PageModule = function PageModule() {};
+
+   PageModule.prototype.saveFile = function(data, mimeType, filename) {
+    const blob = new Blob([data], {
+      type: mimeType
+    });
+    // IE/Edge
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(blob);
+      return;
+    }
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = filename;
+    link.click();
+    // Firefox: delay revoking the ObjectURL
+    setTimeout(function() {
+  
+      URL.revokeObjectURL(blob);
+    }, 100);
+  };
+
+
+
+  return PageModule;
+});
